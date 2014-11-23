80d06d39216b2a880459b18e55f2a3f0dda6ed4a
ota42y <ota42y@gmail.com>
2014-11-23 14:02:32 +0900

    move sendMessage package

--- work/sendMessage/send.go
+++ work/sendMessage/send.go
@@ -1,13 +1,13 @@
-package main
+package sendMessage
 
 import (
-	"./database"
-	"./sendHubot"
+	"../../database"
+	"../../sendHubot"
 )
 
 type SendData struct {
 	Database *database.Database
-	Server *sendHubot.Server
+	Server   *sendHubot.Server
 }
 
 func NewSendDataFromMap(sendDataConfig map[interface{}]interface{}) (sendData *SendData) {
@@ -20,28 +20,17 @@ func NewSendDataFromMap(sendDataConfig map[interface{}]interface{}) (sendData *S
 func NewSendData(dsn string, postPath string, defaultRoomName string) (sendData *SendData) {
 	return &SendData{
 		Database: database.NewDatabase(dsn, defaultRoomName),
-		Server: sendHubot.NewServer(postPath),
+		Server:   sendHubot.NewServer(postPath),
 	}
 }
 
-func (sendData *SendData) SendData(limit int) {
-	noSendPosts := sendData.Database.GetNoSendPosts(limit)
-	if(len(noSendPosts) != 0){
-		for _, post := range noSendPosts{
-			if(sendData.Server.SendData(post.GetUrlValue())){
+func (sendData *SendData) Work() {
+	noSendPosts := sendData.Database.GetNoSendPosts(100)
+	if len(noSendPosts) != 0 {
+		for _, post := range noSendPosts {
+			if sendData.Server.SendData(post.GetUrlValue()) {
 				sendData.Database.SendPost(post)
 			}
-		}
-	}
-}
-
-
-func (sendData *SendData) TestData(){
-	noSendPosts := sendData.Database.GetNoSendPosts(100)
-
-	if(len(noSendPosts) != 0){
-		if(sendData.Server.SendData(noSendPosts[0].GetUrlValue())){
-			sendData.Database.SendPost(noSendPosts[0])
 		}
 	}
 }
