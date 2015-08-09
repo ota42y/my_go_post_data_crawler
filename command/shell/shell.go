56e9c61ec2545ed898e5d3fe5ee02e2e72cc6391
ota42y <ota42y@gmail.com>
2015-08-09 11:50:04 +0900

    add shell command

---
+++ command/shell/shell.go
@@ -0,0 +1,85 @@
+package shell
+
+import (
+	"fmt"
+	"strings"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/BurntSushi/toml"
+	"github.com/mattn/go-shellwords"
+
+	"../command"
+)
+
+type Setting struct {
+	Shells []ShellCommand
+}
+
+type ShellCommand struct {
+	Name string
+	Command string
+	WorkDir string
+}
+
+type Shell struct {
+	commands map[string]ShellCommand
+}
+
+func New(settingText string) (c *Shell) {
+	s := Setting{}
+	_, err := toml.Decode(settingText, &s)
+	if err != nil {
+		return nil
+	}
+
+	shell := &Shell{}
+	shell.commands = make(map[string]ShellCommand)
+
+	for _, commandData := range s.Shells {
+		shell.commands[commandData.Name] = commandData
+	}
+
+	return shell
+}
+
+func (c *Shell) IsExecute(order command.Order) bool {
+	return order.Name == "shell"
+}
+
+func (c *Shell) Execute(order command.Order) string {
+	inputs := strings.Split(order.Data, " ")
+	if 0 < len(inputs) {
+		name := inputs[0]
+		cmd, ok := c.commands[name]
+		if ok {
+			prev, err := filepath.Abs(".")
+			if err != nil {
+				return "prev directory get error"
+			}
+			defer os.Chdir(prev)
+			os.Chdir(cmd.WorkDir)
+
+			var line string
+			if 1 < len(inputs) {
+				line = fmt.Sprintf(cmd.Command, strings.Join(inputs[1:], " "))
+			} else {
+				line = cmd.Command
+			}
+
+			args, err := shellwords.Parse(line)
+			if err != nil {
+				return err.Error()
+			}
+
+			out, err := exec.Command(args[0], args[1:]...).Output()
+			if err != nil {
+				return err.Error()
+			}
+			return strings.Trim(string(out), "\n")
+		}
+	}
+
+	return "no shell command"
+}
---
+++ command/shell/shell_test.go
@@ -0,0 +1,75 @@
+package shell
+
+import (
+    "testing"
+
+    . "github.com/smartystreets/goconvey/convey"
+
+    "../command"
+)
+
+func TestNew(t *testing.T) {
+    settingText := `
+[[shells]]
+name = "test"
+command = "option"
+workDir = "./"
+
+[[shells]]
+name = "testecho"
+command = "echo aaa"
+workDir = "./"
+
+[[shells]]
+name = "catecho"
+command = "echo \"%s\""
+workDir = "./"
+`
+    Convey("New", t, func() {
+        Convey("correct", func() {
+            cmd := New(settingText)
+
+            Convey("load", func() {
+                So(cmd, ShouldNotBeNil)
+                So(len(cmd.commands), ShouldEqual, 3)
+            })
+
+            Convey("command", func() {
+                scmd := cmd.commands["test"]
+                So(scmd, ShouldNotBeNil)
+                So(scmd.Name, ShouldEqual, "test")
+                So(scmd.Command, ShouldEqual, "option")
+                So(scmd.WorkDir, ShouldEqual, "./")
+
+                scmd2 := cmd.commands["testecho"]
+                So(scmd2, ShouldNotBeNil)
+                So(scmd2.Name, ShouldEqual, "testecho")
+                So(scmd2.Command, ShouldEqual, "echo aaa")
+                So(scmd2.WorkDir, ShouldEqual, "./")
+            })
+
+            Convey("execute", func() {
+                Convey("no option", func() {
+                    order := &command.Order{}
+                    order.Data = "testecho"
+                    ret := cmd.Execute(*order)
+                    So(ret, ShouldEqual, "aaa")
+                })
+
+                Convey("with option", func() {
+                    order := &command.Order{}
+                    order.Data = "catecho execute test"
+                    ret := cmd.Execute(*order)
+                    So(ret, ShouldEqual, "execute test")
+                })
+
+                Convey("no command", func() {
+                    order := &command.Order{}
+                    order.Data = "nothing"
+                    ret := cmd.Execute(*order)
+                    So(ret, ShouldEqual, "no shell command")
+                })
+            })
+        })
+    })
+}
